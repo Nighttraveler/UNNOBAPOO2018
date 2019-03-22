8d72e3abcc3e5ef7c3f9e7c3d85a427262b6281a
Fernando Velazquez <fevelazquez@comunidad.unnoba.edu.ar>
2019-03-21 22:45:20 -0300

    Agregar converters para actividades Simples Y Compuestas

--- src/main/java/ar/edu/unnoba/poo2018/converter/ActividadCompuestaConverter.java
+++ src/main/java/ar/edu/unnoba/poo2018/converter/ActividadCompuestaConverter.java
@@ -1,4 +1,13 @@
 package ar.edu.unnoba.poo2018.converter;
 
-public class ActividadCompuestaConverter {
+import ar.edu.unnoba.poo2018.model.ActividadCompuesta;
+
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass = ActividadCompuesta.class)
+public class ActividadCompuestaConverter extends AbstractConverter<ActividadCompuesta> {
+    @Override
+    public String getDAOName() {
+        return "ActividadCompuestaDAO";
+    }
 }
--- src/main/java/ar/edu/unnoba/poo2018/converter/ActividadSimpleConverter.java
+++ src/main/java/ar/edu/unnoba/poo2018/converter/ActividadSimpleConverter.java
@@ -1,4 +1,14 @@
 package ar.edu.unnoba.poo2018.converter;
 
-public class ActividadSimpleConverter {
+
+import ar.edu.unnoba.poo2018.model.ActividadSimple;
+
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass = ActividadSimple.class)
+public class ActividadSimpleConverter extends AbstractConverter<ActividadSimple>{
+    @Override
+    public String getDAOName() {
+        return "ActividadSimpleDAO";
+    }
 }
